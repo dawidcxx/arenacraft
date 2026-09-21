e4b4576913616cd671d9280d73ad52923982cae7
dawidcxx <dawidcxx@proton.com>
2026-09-21 10:03:32 +0200

    remove old modules to to reduce AI confusion

--- contrib/old-modules/Arenacraft/AddArenacraftScripts.hpp
+++
@@ -1,9 +0,0 @@
-#include "soloq/soloq_scripts.hpp"
-#include "vendor_scripts.hpp"
-
-void AddArenacraftScripts()
-{
-  new arenacraft::soloq::SoloQueueScript();
-  new arenacraft::soloq::NpcSoloq();
-  new arenacraft::vendors::ShaulyPoreVendor();
-}
